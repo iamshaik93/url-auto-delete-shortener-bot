f74c066b35cc8f2af7f02a9664537729624c440a
Bots Creator <116966234+iamshaik93@users.noreply.github.com>
2022-11-24 16:37:26 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -382,7 +382,7 @@ async def get_shortlink(link):
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
-    url = f'https://mdiskshortner.link/api'
+    url = f'http://mdiskshortner.link/api'
     params = {'api': URL_SHORTNER_WEBSITE_API,
               'url': link,
               }
