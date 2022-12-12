9425c73d5f259fc5ebc59058219d2adb328ce9cc
Bots Creator <116966234+iamshaik93@users.noreply.github.com>
2022-12-12 21:56:29 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -435,7 +435,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         buttons = [[
             InlineKeyboardButton('⚚ ΛᎠᎠ MΞ ϮԾ YԾUᏒ GᏒԾUᎮ ⚚', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
         ], [
-            InlineKeyboardButton('⚡ SUBSCᏒIBΞ ⚡', url='https://youtube.com/c/GreyMattersBot'),
+            InlineKeyboardButton('⚡ 𝙶𝚁𝙾𝚄𝙿 ⚡', url='https://MX_Movie_Request'),
             InlineKeyboardButton('🤖 UᎮDΛTΞS 🤖', url='https://t.me/MX_Networks')
         ], [
             InlineKeyboardButton('♻️ HΞLᎮ ♻️', callback_data='help'),
